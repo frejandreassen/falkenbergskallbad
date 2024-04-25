f8d3cf0c75d823097d3175378ff9e3e12760a9de
frejandreassen <frej.andreassen@gmail.com>
2024-04-25 17:20:05 +0200

    Use next/link in footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
   
   export default function Footer({navigation, facebook, instagram, copyright_text}) {
     return (
@@ -6,9 +8,9 @@
           <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
             {navigation.map((item) => (
               <div key={item.name} className="pb-6">
-                <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+                <Link href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
                   {item.name}
-                </a>
+                </Link>
               </div>
             ))}
           </nav>

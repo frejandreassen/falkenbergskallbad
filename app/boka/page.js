bccd827cf594f624bbdc1f9798080801983fdd64
frejandreassen <frej.andreassen@gmail.com>
2024-10-17 15:10:10 +0200

    Reorganize the boka components

--- app/boka/page.js
+++ app/boka/page.js
@@ -1,19 +1,17 @@
-import { getSlots, getPriceList } from '@/lib/actions'
-import Booking from '@/components/Booking'
-
-
+import { getSlots, getPriceList } from "@/lib/actions";
+import Booking from "@/components/Boka/Booking";
 
 export default async function Page() {
-// const [checkoutOpen, setCheckoutOpen] = useState(false)
-const slots = await getSlots()
-const priceList = await getPriceList()
+  // const [checkoutOpen, setCheckoutOpen] = useState(false)
+  const slots = await getSlots();
+  const priceList = await getPriceList();
 
   return (
     <div className="bg-white py-24 px-4 max-w-5xl mx-auto">
-    
-    <h1 className="font-bodoni-moda text-3xl my-10 hidden lg:block">Boka bastu</h1>
-    <Booking slots={slots} priceList={priceList}/>
-    
+      <h1 className="font-bodoni-moda text-3xl my-10 hidden lg:block">
+        Boka bastu
+      </h1>
+      <Booking slots={slots} priceList={priceList} />
     </div>
-  )
+  );
 }
